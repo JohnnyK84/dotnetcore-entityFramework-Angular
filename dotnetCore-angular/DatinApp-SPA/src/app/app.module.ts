d4d95201599d39af6eb3320cb1faa38ba5503b94
Johnny Kingi <johnny.kingi@rezare.com>
2019-12-12 14:07:04 +1300

    End of section 10. Created component for editing profile, CanDeactivate Route Guard not working @HostListener decorator not playing ball, Updated user profiles persist to API

--- dotnetCore-angular/DatinApp-SPA/src/app/app.module.ts
+++ dotnetCore-angular/DatinApp-SPA/src/app/app.module.ts
@@ -26,6 +26,9 @@ import { MemberDetailComponent } from "./members/member-detail/member-detail.com
 import { TabsModule } from "ngx-bootstrap/tabs";
 import { MemberDetailResolver } from "./_resolvers/member-detail.resolver";
 import { MemberListResolver } from "./_resolvers/member-list.resolver";
+import { MemberEditComponent } from "./members/member-edit/member-edit.component";
+import { MemberEditResolver } from "./_resolvers/member-edit.resolver";
+import { PreventUnsavedChanges } from "./_guards/prevent-unsaved-changes.guard";
 
 // function for JWTModule to attach JWT Tokens to HttpRequests
 export function tokenGetter() {
@@ -50,7 +53,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
     ListsComponent,
     MessagesComponent,
     MemberCardComponent,
-    MemberDetailComponent
+    MemberDetailComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
@@ -73,6 +77,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
     ErrorInterceptorProvider,
     MemberDetailResolver,
     MemberListResolver,
+    MemberEditResolver,
+    PreventUnsavedChanges,
     { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig } // To fix issue with NGX Gallery error
   ],
   bootstrap: [AppComponent]
---
+++ dotnetCore-angular/DatinApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, ViewChild, HostListener } from "@angular/core";
+import { User } from "src/app/_models/user";
+import { Router, ActivatedRoute } from "@angular/router";
+import { AlertifyService } from "src/app/_services/alertify.service";
+import { NgForm } from "@angular/forms";
+import { UserService } from "src/app/_services/user.service";
+import { AuthService } from "src/app/_services/auth.service";
+
+@Component({
+  selector: "app-member-edit",
+  templateUrl: "./member-edit.component.html",
+  styleUrls: ["./member-edit.component.css"]
+})
+export class MemberEditComponent implements OnInit {
+  @ViewChild("editForm", { static: true }) editForm: NgForm;
+  user: User;
+
+  @HostListener("window:beforeunload", ["$event"]) //not working? tab still closes without warning
+  beforeUnloadHandler($event: any) {
+    if (this.editForm.dirty) {
+      $event.retrunValue = true;
+    }
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private alertify: AlertifyService,
+    private userService: UserService,
+    private authSservice: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.user = data["user"];
+    });
+  }
+
+  updateUser() {
+    this.userService
+      .updateUser(this.authSservice.decodedToken.nameid[0], this.user)
+      .subscribe(
+        next => {
+          this.alertify.success("Profile updated succesfully");
+          this.editForm.reset(this.user);
+        },
+        err => {
+          this.alertify.error(err);
+        }
+      );
+  }
+}
